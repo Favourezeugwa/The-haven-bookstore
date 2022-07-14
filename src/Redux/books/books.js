3a0e08fbfcc6e6dae7955360a693a571c98a3411
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-14 18:02:52 +0100

    Add functionality for add and remove

--- src/Redux/books/books.js
+++ src/Redux/books/books.js
@@ -9,7 +9,27 @@ export const removeBook = (id) => ({
   type: REMOVE, id,
 });
 
-const initialState = [];
+const initialState = [{
+  id: 1,
+  title: 'The hunger games',
+  author: 'Gold Smith',
+  percentageCompleted: 64,
+  currentChapter: 17,
+},
+{
+  id: 2,
+  title: 'Dune',
+  author: 'Meyer',
+  percentageCompleted: 8,
+  currentChapter: 3,
+},
+{
+  id: 3,
+  title: 'Capital in the 21st century',
+  author: 'Smith',
+  percentageCompleted: 0,
+  currentChapter: 1,
+}];
 
 const booksReducer = (state = initialState, action = {}) => {
   switch (action.type) {
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -2,11 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const Store = configureStore({
+const store = configureStore({
   reducer: {
     books: booksReducer,
     categories: categoriesReducer,
   },
 });
 
-export default Store;
+export default store;
