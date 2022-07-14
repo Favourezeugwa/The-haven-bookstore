fc26816acad348552a0028561bfb00af1de0763e
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-14 18:40:17 +0100

    Add functionality for checkStatus

--- src/Components/BookContainer.js
+++ src/Components/BookContainer.js
@@ -4,6 +4,7 @@ import BookList from './BookList';
 import Form from './Form';
 
 const BookContainer = () => {
+// declare a global state variable for books and configureStore
   const books = useSelector((state) => state.books);
 
   return (
--- src/Components/Categories.js
+++ src/Components/Categories.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { checkStatus } from '../Redux/categories/categories';
 
-const Categories = () => (
-  <>
-    <button type="button">Check Status</button>
-  </>
-);
+const Categories = () => {
+  const dispatch = useDispatch();
 
+  // declare a global state variable from configureStore categories
+  const status = useSelector((state) => state.categories);
+
+  const handleClick = () => {
+    dispatch(checkStatus());
+  };
+
+  return (
+    <>
+      <button
+        type="button"
+        onClick={handleClick}
+      >
+        Check Status
+      </button>
+      <p>{status}</p>
+    </>
+  );
+};
 export default Categories;
