cb26779d966fea0c6f1b864818b0d7d54fc6a0ae
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-15 14:12:27 +0100

    add thunk and applyMiddleWare

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import store from './Redux/configureStore';
+
 import Navigation from './Components/Navigation';
 import BookContainer from './Components/BookContainer';
 import Categories from './Components/Categories';
@@ -11,13 +10,11 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Provider store={store}>
-          <Navigation />
-          <Routes>
-            <Route exact path="/" element={<BookContainer />} />
-            <Route exact path="/categories" element={<Categories />} />
-          </Routes>
-        </Provider>
+        <Navigation />
+        <Routes>
+          <Route exact path="/" element={<BookContainer />} />
+          <Route exact path="/categories" element={<Categories />} />
+        </Routes>
       </Router>
     </div>
   );
--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
+import thunk from 'redux-thunk';
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
 
-const store = configureStore({
-  reducer: {
-    books: booksReducer,
-    categories: categoriesReducer,
-  },
-});
+const store = configureStore(
+  {
+    reducer: {
+      books: booksReducer,
+      categories: categoriesReducer,
+    },
+  }, applyMiddleware(thunk),
+);
 
 export default store;
