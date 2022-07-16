c059b2c79a5e8f3825361ca5a6584bc2dd0565e2
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-16 17:33:29 +0100

    changes based on review

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -3,25 +3,32 @@ import { Link } from 'react-router-dom';
 import './styles/navigation.css';
 import { FaUser } from 'react-icons/fa';
 
-const Navigation = () => (
-  <nav>
-    <div className="navLogo">
-      <h1 className="logo-title">Bookstore CMS</h1>
-    </div>
-    <ul className="navLinks">
-      <li>
-        <Link to="/">
-          BOOKS
-        </Link>
-      </li>
-      <li>
-        <Link to="/categories">
-          CATEGORIES
-        </Link>
-      </li>
-    </ul>
-    <FaUser className="user-icon" style={{ color: '#0290ff', cursor: 'pointer' }} />
-  </nav>
-);
+const Navigation = () => {
+  const iconStyle = {
+    color: '#0290ff',
+    cursor: 'pointer',
+  };
+
+  return (
+    <nav>
+      <div className="navLogo">
+        <h1 className="logo-title">Bookstore CMS</h1>
+      </div>
+      <ul className="navLinks">
+        <li>
+          <Link to="/">
+            BOOKS
+          </Link>
+        </li>
+        <li>
+          <Link to="/categories">
+            CATEGORIES
+          </Link>
+        </li>
+      </ul>
+      <FaUser className="user-icon" style={iconStyle} />
+    </nav>
+  );
+};
 
 export default Navigation;
