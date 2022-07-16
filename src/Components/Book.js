35627ce34e2db47be0405524aeb37fd8a7ae4122
Favourezeugwa <favourezeugwa@gmail.com>
2022-07-16 12:47:43 +0100

    Modify navigation.css file

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useDispatch } from 'react-redux';
 import { deleteBook } from '../Redux/books/books';
+import './styles/book.css';
 
 const Book = (props) => {
   const { id, book } = props;
